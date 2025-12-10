073f0a336a292658e79655fd28b5496b853fbdd5
beginner874 <davideyu4@gmail.com>
2025-12-10 07:51:52 +0000

    richiede dati obbligatori

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main () {
-    printf("ciao\n");
+    float L [] = {0,0,0};
+    float perimetro;
+    printf("inserisci i dati del triangolo\n");
+    printf("segmento AB\n");
+    scanf("%f",&L[0]);
+    printf("segmento BC\n");
+    scanf("%f",&L[1]);
+    printf("segmento CA\n");
+    scanf("%f",&L[2]);
+    perimetro=L[0]+L[1]+L[2];
+    printf("%.3f\n",perimetro);
     return 0;
 }
