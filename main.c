c92935a6ecc66fba162b0e3dfa75f236bd9a8172
beginner874 <davideyu4@gmail.com>
2025-12-16 18:54:48 +0000

    controlla che triangolo è

--- main.c
+++ main.c
@@ -39,7 +39,20 @@ int main () {
         A[1]=A[1]*180/3.1415926;
         A[2]=A[2]*180/3.1415926;
         perimetro=L[0]+L[1]+L[2];
-        area=(L[0]*L[1]*A[0])/2
+        area=(L[0]*L[1]*A[0])/2;
+        if (A[0]==90||A[1]==90||A[2]==90) {
+            printf("e un triangolo rettangolo\n");
+            
+        }
+        else if (A[0]==A[1]||A[0]==A[2]||A[1]==A[2]&&(A[0]!=45||A[1]!=45)) {
+            printf("è un triangolo isoscele\n");
+        }
+        else if (A[0]==A[1]&&A[0]==A[2]&&A[1]==A[2]) {
+            printf("è un triangolo equilatero\n");
+        }
+        else {
+            printf("è un triangolo scaleno\n");
+        }
         printf("angolo alfa è: %.3f\n",A[0]);
         printf("angolo beta è: %.3f\n",A[1]);
         printf("angolo gamma è: %.3f\n",A[2]);
